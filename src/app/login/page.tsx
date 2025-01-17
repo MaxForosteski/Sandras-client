8a36cd6237c76c92e183c1e2c42124e6a748fde8
Max <maxforosteski12@gmail.com>
2025-01-16 23:31:46 -0300

    first commit

---
+++ src/app/login/page.tsx
@@ -0,0 +1,25 @@
+import Image from "next/image";
+
+
+export default function Login() {
+    return (
+        <div className="main flex ">
+            <div className="coluna-2 w-5/12 bg-neutral-50 h-screen">
+                <div className="flex flex-col justify-center items-center h-full">
+                    <h1 className="text-4xl ">Login</h1>
+                    
+                </div>
+            </div>
+            <div className="coluna-1 w-7/12 h-screen">
+                <Image
+                    src="/images/login-cat.jpg"
+                    alt="Gato na janela"
+                    width={0}
+                    height={0}
+                    layout="responsive"
+                    objectFit="cover"
+                />
+            </div>
+        </div>
+    )
+}
